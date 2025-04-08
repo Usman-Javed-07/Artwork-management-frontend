46c5586aa59968149411487dae6c399947f82ffd
Usman Javed <usmanjaved0816@gmail.com>
2025-04-08 13:14:00 +0500

    update detail list style

--- src/pages/ArtworkDetailPage/ArtworkDetail.jsx
+++ src/pages/ArtworkDetailPage/ArtworkDetail.jsx
@@ -22,16 +22,23 @@ const ArtworkDetails = () => {
         <img
           src={`http://localhost:5000${artwork.pictureUrl}`}
           alt={artwork.title}
+          style={{ width: '600px', height: '400px' }}
         />
       </div>
       <div className={styles.artworkDetailList}>
-        <h2>{artwork.title}</h2>
-        <p>
-          <strong>Artist:</strong> {artwork.artist}
-        </p>
-        <p>
-          <strong>Year:</strong> {artwork.year}
-        </p>
+        <h1>{artwork.title}</h1>     
+          <h2>{artwork.artist}</h2>
+        <strong>{artwork.year}</strong>
+        <p>{artwork.technique}</p>
+        <p>{artwork.subTechnique}</p>
+        <p>{artwork.medium}</p>
+        <p>{artwork.numberOfEditions}</p>
+        <p>{artwork.editionNumber}</p>
+        <p>{artwork.provenance}</p>
+        <p>{artwork.literatureList}</p>
+        <p>{artwork.exhibitionList}</p>
+        <p>{artwork.extraInformation}</p>
+        
       </div>
     </div>
   );
